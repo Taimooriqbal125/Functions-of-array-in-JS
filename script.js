c85e1362a51071ddb7ea68f74002c31fd467f460
Taimooriqbal125 <taimooriqbal125@gmail.com>
2024-06-03 00:12:59 -0700

    Function of array in Js

---
+++ script.js
@@ -0,0 +1,12 @@
+//  Built in functions of array in JS
+
+//  Push function (use to add new element in end of array )
+// syntax   nameofarray.push()
+var temii = ["ali" , "temiii" ,"abu" ,"ami"];
+temii.push("baji", "khala");
+console.log("Original" , temii);
+
+// Unshift function (use to add new elemet at starting of array)
+//  syntax  nameofarray.unshift( data we want to enter )
+temii.unshift("saeed" ,"hurara");
+console.log("Original" , temii);
